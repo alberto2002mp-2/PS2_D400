5f5f554bf00c4af734b21244aa90ed29511e9420
alberto2002mp-2 <alberto2002mp@gmail.com>
2025-11-04 22:00:50 +0000

    deleted old file

--- code.py
+++
@@ -1,8 +0,0 @@
-# Define the number we want the factors of
-n = 362880
-
-# Use a list comprehension to generate all pairs of factors
-factor_pairs = [(i, n // i) for i in range(1, int(n**0.5) + 1) if n % i == 0]
-
-# Display the result
-print(factor_pairs)
